4eba77e2db6ef5e5391d1c8875e9a65437b6be52
Adam Rupe <atrupe@ucdavis.edu>
2026-06-15 16:30:36 -0700

    use absolute paths

--- scripts/run-preempt.sh
+++ scripts/run-preempt.sh
@@ -14,8 +14,8 @@
 #SBATCH --module=gpu,nccl-plugin
 #SBATCH --requeue
 #SBATCH --open-mode=append
-#SBATCH -o ../slurm/stft-ddp-%j.out
-#SBATCH -e ../slurm/stft-ddp-%j.err
+#SBATCH -o /pscratch/sd/a/atrupe/StFT/slurm/stft-ddp-%j.out
+#SBATCH -e /pscratch/sd/a/atrupe/StFT/slurm/stft-ddp-%j.err
 #SBATCH --mail-type=END,FAIL,REQUEUE
 #SBATCH --mail-user=adamrupe@lbl.gov
 
@@ -27,7 +27,7 @@ export MASTER_PORT="${MASTER_PORT:-$((10000 + SLURM_JOB_ID % 50000))}"
 
 srun --cpu-bind=cores shifter bash -c \
     'unset NCCL_CROSS_NIC; exec python "$@"' bash \
-    ../train.py
+    /pscratch/sd/a/atrupe/StFT/train.py
 
 status=$?
 if [ "$status" -ne 0 ]; then
